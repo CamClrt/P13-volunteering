fbbd988a63b8a137a0d4f28bae58f7334998f5a3
CamClrt <camille.clarret@gmail.com>
2021-07-06 15:22:52 +0200

    test(home + users): refactor tests + test registration

--- src/home/tests/integration/tests_views.py
+++ src/home/tests/integration/tests_views.py
@@ -1,11 +1,8 @@
-from django.test import Client, TestCase
+from django.test import TestCase
 from django.urls import reverse
 
 
 class TestHomeView(TestCase):
-    def setUp(self):
-        self.client = Client()
-
     def test_display_home_page(self):
         response = self.client.get(reverse("home:home"))
         self.assertEqual(response.status_code, 200)
--- src/users/tests/integration/tests_views.py
+++ src/users/tests/integration/tests_views.py
@@ -1,22 +1,47 @@
-from django.test import Client, TestCase
+from django.test import TestCase
 from django.urls import reverse
 
 from users.models import CustomUser, Status
 
 
 class TestRegisterView(TestCase):
-    def setUp(self):
-        self.client = Client()
-
     def test_registration_get(self):
         response = self.client.get(reverse("users:register"))
         self.assertTemplateUsed(response, "users/register.html")
         self.assertEqual(response.status_code, 200)
 
+    def test_registration_ok(self):
+        data = {
+            "status": "13",
+            "first_name": "John",
+            "last_name": "Doe",
+            "email": "john.doe@gmail.com",
+            "password1": "1234AZERTY$",
+            "password2": "1234AZERTY$",
+        }
+
+        Status.objects.create(name="Fake status")
+        response = self.client.post(reverse("users:register"), data)
+        self.assertEqual(CustomUser.objects.count(), 1)
+        self.assertRedirects(response, "/users/login/")
+
+    def test_registration_fail(self):
+        data = {
+            "status": "12",
+            "first_name": "John",
+            "last_name": "Doe",
+            "password1": "1234AZERTY$",
+            "password2": "1234AZERTY$",
+        }
+
+        Status.objects.create(name="Other Fake status")
+        response = self.client.post(reverse("users:register"), data)
+        self.assertEqual(CustomUser.objects.count(), 0)
+        self.assertEqual(response.status_code, 200)
+
 
 class TestProfileView(TestCase):
     def setUp(self):
-        self.client = Client()
         CustomUser.objects.create_user(
             email="john.doe@gmail.com",
             first_name="John",
