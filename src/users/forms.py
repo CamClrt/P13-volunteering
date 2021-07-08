36272d25550d08e85aef6b575c0846514a185415
CamClrt <camille.clarret@gmail.com>
2021-07-08 21:08:24 +0200

    feat(profile): create profile form + style + links

--- src/users/forms.py
+++ src/users/forms.py
@@ -1,13 +1,19 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 
-from .models import CustomUser
+from .models import (  # isort:skip
+    Address,
+    CandidateProfile,
+    City,
+    CustomUser,
+    OrganizationProfile,
+)
 
 
 class UserRegistrationForm(UserCreationForm):
     class Meta:
 
-        CHOICES = [
+        STATUS = [
             ("1", "Un bénévole"),
             ("2", "Une association"),
         ]
@@ -30,5 +36,91 @@ class UserRegistrationForm(UserCreationForm):
             "password2": "Confirmation du mot de passe",
         }
         widgets = {
-            "status": forms.RadioSelect(choices=CHOICES),
+            "status": forms.RadioSelect(choices=STATUS),
+        }
+
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = CustomUser
+        fields = [
+            "last_name",
+            "first_name",
+        ]
+        labels = {
+            "first_name": "Votre prénom",
+            "last_name": "Votre nom",
+        }
+
+
+class CandidateProfileForm(forms.ModelForm):
+    class Meta:
+        model = CandidateProfile
+        fields = [
+            "web_site_url",
+            "linkedin_url",
+            "github_url",
+            "gitlab_url",
+            "description",
+        ]
+        labels = {
+            "web_site_url": "Site personnel",
+            "linkedin_url": "Linkedin",
+            "github_url": "GitHub",
+            "gitlab_url": "GitLab",
+            "description": "Bio",
+        }
+
+
+class OrganizationProfileForm(forms.ModelForm):
+    class Meta:
+        model = OrganizationProfile
+        fields = [
+            "name",
+            "sector",
+            "description",
+            "rna_code",
+            "siret_code",
+            "email",
+            "phone_number",
+            "web_site_url",
+        ]
+        labels = {
+            "name": "Dénomination de la structure",
+            "sector": "Secteur d'activité",
+            "description": "Description",
+            "rna_code": "Code RNA",
+            "siret_code": "SIRET",
+            "email": "Mail de contact",
+            "phone_number": "Téléphone",
+            "web_site_url": "Site Web",
+        }
+        widgets = {
+            "sector": forms.RadioSelect(),
+        }
+
+
+class AddressForm(forms.ModelForm):
+    class Meta:
+        model = Address
+        fields = [
+            "address_1",
+            "address_2",
+        ]
+        labels = {
+            "address_1": "Adresse",
+            "address_2": "Complément d'adresse",
+        }
+
+
+class CityForm(forms.ModelForm):
+    class Meta:
+        model = City
+        fields = [
+            "name",
+            "zip_code",
+        ]
+        labels = {
+            "name": "Ville",
+            "zip_code": "Code postale",
         }
--- src/users/views.py
+++ src/users/views.py
@@ -1,7 +1,18 @@
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
 
-from .forms import UserRegistrationForm
+from .models import Address, CandidateProfile, City, OrganizationProfile
+
+from .forms import (  # isort:skip
+    AddressForm,
+    CandidateProfileForm,
+    CityForm,
+    OrganizationProfileForm,
+    UserForm,
+    UserRegistrationForm,
+)
 
 
 def register(request):
@@ -18,4 +29,59 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, "users/profile.html")
+    if request.method == "POST":
+        user = request.user
+
+        try:
+            candidate = CandidateProfile.objects.get(user=user)
+        except ObjectDoesNotExist:
+            pass
+
+        try:
+            organization = OrganizationProfile.objects.get(user=user)
+        except ObjectDoesNotExist:
+            pass
+
+        return HttpResponseRedirect(request.path)
+
+    else:
+        user = request.user
+
+        try:
+            candidate = CandidateProfile.objects.get(user=user)
+            address = Address.objects.get(id=candidate.location.id)
+            city = City.objects.get(id=address.city.id)
+
+            if candidate:
+                context = {
+                    "profile": candidate,
+                    "candidate_profile_form": CandidateProfileForm(
+                        instance=candidate,
+                    ),
+                    "city_form": CityForm(instance=city),
+                    "address_form": AddressForm(instance=address),
+                }
+        except ObjectDoesNotExist:
+            pass
+
+        try:
+            organization = OrganizationProfile.objects.get(user=user)
+            # sector = Sector.objects.get(id=organization.sector.id)
+            address = Address.objects.get(id=organization.location.id)
+            city = City.objects.get(id=address.city.id)
+
+            if organization:
+                context = {
+                    "profile": organization,
+                    "organization_profile_form": OrganizationProfileForm(
+                        instance=organization,
+                    ),
+                    "city_form": CityForm(instance=city),
+                    "address_form": AddressForm(instance=address),
+                }
+        except ObjectDoesNotExist:
+            pass
+
+        context["user_form"] = UserForm(instance=user)
+
+        return render(request, "users/profile.html", context)
