46542521ec9ed6af7e3794496b9520544bbc0895
CamClrt <camille.clarret@gmail.com>
2021-07-12 16:20:47 +0200

    feat(model): add Activity relation

--- src/candidate/models.py
+++ src/candidate/models.py
@@ -1,3 +1,50 @@
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+
+class Activity(models.Model):
+    ACTIVITY_CHOICES = [
+        (
+            "autres",
+            "Autres",
+        ),
+        ("base-de-donnees", "Base de données,"),
+        ("developpement-back-end", "Développement Back-end,"),
+        ("developpement-front-end", "Développement Front-end,"),
+        ("developpement-full-stack", "Développement Full-Stack,"),
+        ("gestion-de-projet", "Gestion de projet"),
+        (
+            "gestion-de-site-reseaux-sociaux",
+            "Gestion de site & réseaux sociaux",
+        ),
+        (
+            "marketing",
+            "Marketing",
+        ),
+        (
+            "produit",
+            "Produit",
+        ),
+        (
+            "pedagogie-formation",
+            "Pédagogie & formation",
+        ),
+        (
+            "seosea",
+            "SEO/SEA",
+        ),
+        (
+            "support",
+            "Support",
+        ),
+        (
+            "systeme",
+            "Système",
+        ),
+    ]
+
+    name = models.CharField(
+        max_length=(50),
+        unique=True,
+        blank=True,
+        choices=ACTIVITY_CHOICES,
+    )
