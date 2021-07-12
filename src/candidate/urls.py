1e30670056a6b28e223d38885455b01dcee1dc41
CamClrt <camille.clarret@gmail.com>
2021-07-12 19:17:02 +0200

    feat(candidate): add views + templates + tests to update candidate dashboard

--- src/candidate/urls.py
+++ src/candidate/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
 
-from .views import dashboard
+from .views import activity, availability, dashboard, wish
 
 app_name = "candidate"
 
 urlpatterns = [
-    path(
-        "dashboard/",
-        dashboard,
-        name="dashboard",
-    ),
+    path("dashboard/", dashboard, name="dashboard"),
+    path("activity/", activity, name="activity"),
+    path("availability/", availability, name="availability"),
+    path("wish/", wish, name="wish"),
 ]
