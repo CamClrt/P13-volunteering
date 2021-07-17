ef7c8ddc167aa2c8c24e5a5405956eb1b9fb2cc6
CamClrt <camille.clarret@gmail.com>
2021-07-17 22:39:00 +0200

    refactor(model): Activity + update cmd + template

--- src/candidate/management/commands/create_activity.py
+++ src/candidate/management/commands/create_activity.py
@@ -14,8 +14,23 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         logging.info("Create activities in database")
         try:
-            for activity in Activity.ACTIVITY_CHOICES:
-                Activity.objects.create(name=activity[0])
+            Activity.objects.bulk_create(
+                [
+                    Activity(name="autres"),
+                    Activity(name="base-de-donnees"),
+                    Activity(name="developpement-back-end"),
+                    Activity(name="developpement-front-end"),
+                    Activity(name="developpement-full-stack"),
+                    Activity(name="gestion-de-projet"),
+                    Activity(name="gestion-de-site-reseaux-sociaux"),
+                    Activity(name="marketing"),
+                    Activity(name="produit"),
+                    Activity(name="pedagogie-formation"),
+                    Activity(name="seo-sea"),
+                    Activity(name="support"),
+                    Activity(name="systeme"),
+                ]
+            )
         except IntegrityError:
             logging.warning("activity data already existed")
         logging.info("activity data insertion in DB: finished")
