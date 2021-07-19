3afd76bddae45f8d66738a0cbb9c66403c13fdce
CamClrt <camille.clarret@gmail.com>
2021-07-19 10:53:26 +0200

    feat(views): add dashboard + candidate details views

--- src/organization/views.py
+++ src/organization/views.py
@@ -1,3 +1,15 @@
-# from django.shortcuts import render
+from django.views.generic import DetailView, ListView
 
-# Create your views here.
+from users.models import CandidateProfile
+
+
+class Dashboard(ListView):
+    model = CandidateProfile
+    template_name = "organization/dashboard.html"
+    context_object_name = "candidates"
+
+
+class CandidateDetail(DetailView):
+    model = CandidateProfile
+    template_name = "organization/details.html"
+    context_object_name = "candidate"
