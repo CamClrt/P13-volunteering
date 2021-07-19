985cd1a0ca03f2b108680efbfc9bd913206e7c59
CamClrt <camille.clarret@gmail.com>
2021-07-19 10:53:26 +0200

    feat(views): add dashboard + candidate details views

--- src/organization/urls.py
+++ src/organization/urls.py
@@ -1,5 +1,18 @@
-# from django.urls import path
+from django.urls import path
+
+from .views import CandidateDetail, Dashboard
 
 app_name = "organization"
 
-urlpatterns = []
+urlpatterns = [
+    path(
+        "dashboard/",
+        Dashboard.as_view(),
+        name="dashboard",
+    ),
+    path(
+        "details/<int:candidate_id>/",
+        CandidateDetail.as_view(),
+        name="details",
+    ),
+]
