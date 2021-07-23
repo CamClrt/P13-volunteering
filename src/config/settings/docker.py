fab93517121b4f95af9194fa19a9ce8a24c7b15d
CamClrt <camille.clarret@gmail.com>
2021-07-23 07:53:51 +0200

    chore(deployment): Docker + NGINX config

--- src/config/settings/docker.py
+++ src/config/settings/docker.py
@@ -3,7 +3,7 @@ import os
 from .base import *  # noqa: F401, F403
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "localhost",
@@ -16,13 +16,14 @@ ALLOWED_HOSTS = [
 DATABASES = {
     "default": {
         "ENGINE": os.environ.get("ENGINE"),
-        "NAME": os.environ.get("NAME"),
-        "USER": os.environ.get("USER"),
-        "PASSWORD": os.environ.get("PASSWORD"),
-        "HOST": os.environ.get("HOST"),
-        "PORT": os.environ.get("PORT"),
+        "NAME": os.environ.get("SQL_DATABASE"),
+        "USER": os.environ.get("SQL_USER"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD"),
+        "HOST": os.environ.get("SQL_HOST"),
+        "PORT": os.environ.get("SQL_PORT"),
     }
 }
+
 
 INSTALLED_APPS += [  # noqa: F405
     # https://django-extensions.readthedocs.io/en/latest/
@@ -34,3 +35,9 @@ INSTALLED_APPS += [  # noqa: F405
 MIDDLEWARE += [  # noqa: F405
     "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
+
+STATIC_URL = "/staticfiles/"
+STATIC_ROOT = BASE_DIR / "staticfiles"  # noqa: F405
+
+MEDIA_URL = "/mediafiles/"
+MEDIA_ROOT = BASE_DIR / "mediafiles"  # noqa: F405
